744a6ca4962f8501d3538063b403faab06e5366d
CAD Client <CAD Client>
2016-03-09 20:23:06 +0900

    Lee Se Dol : Alpha Go

--- ex01/ex01.py
+++ ex01/ex01.py
@@ -1,10 +1,11 @@
 print "Hello World"
-print "Computer Programming Fundamentals"
-print "I like typing this"
-print "This is fun"
-print 'Yay! Printing'
-print "I'd much rather you 'not'."
-print 'i "said" do not touch this.'
+ print "Computer Programming Fundamentals"
+ print "I like typing this"
+ print "This is fun"
+ print 'Yay! Printing'
+ print "I'd much rather you 'not'."
+ print 'i "said" do not touch this.'
 
-print "Jiyun Kim"
-print "2016 03 09"
+ print "Jiyun Kim"
+ print "2016 03 09"
+ print "Lee Se Dol : Alpha Go = 0:1"
