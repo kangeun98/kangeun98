834788ca54de7b0d6d5bb6b343f5f7411b0d14bf
CAD Client <CAD Client>
2016-03-09 19:47:05 +0900

    added ex01.py

---
+++ ex01/ex01.py
@@ -0,0 +1,7 @@
+print "Hello World"
+print "Computer Programming Fundamentals"
+print "I like typing this"
+print "This is fun"
+print 'Yay! Printing'
+print "I'd much rather you 'not'."
+print 'i "said" do not touch this.'
