3c93420dead6564be58166e8d6e3683ae0e04a85
CAD Client <CAD Client>
2016-03-16 20:09:46 +0900

    added ex03.py

---
+++ ex03/ex03/ex03.py
@@ -0,0 +1,25 @@
+Python 2.7.11 |Anaconda 2.5.0 (64-bit)| (default, Jan 29 2016, 14:26:21) [MSC v.1500 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> #암탉의 수를 세어 봅시다
+>>> 
+>>> print"i will now count my chickens:"
+i will now count my chickens:
+>>> print"Hens",24+30/6
+Hens 29
+>>> print"Roosters",100-25*3%4
+Roosters 97
+>>> print"Now I will count the eggs:"
+Now I will count the eggs:
+>>> print"is it true that 3+2<5-7??
+SyntaxError: EOL while scanning string literal
+>>> print"is it true that 3+2<5-7?"
+is it true that 3+2<5-7?
+>>> print 3+2<5-7
+False
+>>> print"waht is 3+2?",3+2
+waht is 3+2? 5
+>>> print"what is 5-7?",5-7
+what is 5-7? -2
+>>> print"Oh,that's why it's false."
+Oh,that's why it's false.
+>>> 
