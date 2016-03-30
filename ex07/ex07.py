97c08980be2b0d53b08209a3caec5b1243dd225b
CAD Client <CAD Client>
2016-03-30 18:12:40 +0900

    added to ex07.py

---
+++ ex07/ex07.py
@@ -0,0 +1,31 @@
+Python 2.7.11 |Anaconda 2.5.0 (64-bit)| (default, Jan 29 2016, 14:26:21) [MSC v.1500 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print "Mary had a little lamb."
+Mary had a little lamb.
+>>> print "iTs fleece was white as %s." % 'snow'
+iTs fleece was white as snow.
+>>> print "And everywhere that mary went."
+And everywhere that mary went.
+>>> print "." * 10 # waht'd that do?
+..........
+>>> end1="C"
+>>> end2="h'
+SyntaxError: EOL while scanning string literal
+>>> end2="h"
+>>> end3="e"
+>>> end4="e"
+>>> end5="s"
+>>> end6="e"
+>>> end7="B"
+>>> end8="u"
+>>> end9="r"
+>>> end10="g"
+>>> end11="e"
+>>> end12="r"
+>>> 
+>>> # watch that comma at the end. try removing it to see what happens
+>>> print end1+end2+end3+end4+end5+end6,
+Cheese
+>>> print end7+end8+end9+end10+end11+end12
+Burger
+>>> 
