c9f97ebc8386e711711d5d89a3f859db1661b9c9
CAD Client <CAD Client>
2016-04-06 18:17:11 +0900

    added to ex12.py

---
+++ ex12/ex12.py
@@ -0,0 +1,12 @@
+Python 2.7.11 |Anaconda 2.5.0 (64-bit)| (default, Jan 29 2016, 14:26:21) [MSC v.1500 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> age = raw_input("How old are you? ")
+How old are you? 23
+>>> height = raw_input("How tall are you? ")
+How tall are you? 175
+>>> weight = raw_input("How much do you weigh? ")
+How much do you weigh? 67
+>>> print "So, you're %r old, %r tall and %r heavy." % (
+    age, height, weight)
+So, you're '23' old, '175' tall and '67' heavy.
+>>> 
